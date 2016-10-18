a130d58837b20de47d517cf38a8e4019b259144d
Ethan Pailes <ethanpailes@gmail.com>
2016-10-18 13:13:56 -0400

    made basic typescript project skeleton. With @abdisalan

---
+++ scripts/hello-typescript/GruntFile.js
@@ -0,0 +1,24 @@
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+ 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        typescript: {
+            base: {
+                src: ['lib/**/*.ts'],
+                dest: 'js/HelloTypescript.js',
+                options: {
+                    module: 'amd',
+                    target: 'es5'
+                }
+            }
+        },
+        watch: {
+            files: '**/*.ts',
+            tasks: ['typescript']
+        }
+    });
+ 
+    grunt.registerTask('default', ['watch']);
+}
