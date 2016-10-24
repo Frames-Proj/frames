4ba1408d7d0b638fa20fbb742f3e730dee8f38b8
Ethan Pailes <ethanpailes@gmail.com>
2016-10-24 18:46:38 -0400

    more work. It has not yet printed out the filecontents that I store on the network.

--- scripts/ethan-talk-to-mock/lib/auth-and-store.ts
+++ scripts/ethan-talk-to-mock/lib/auth-and-store.ts
@@ -3,6 +3,8 @@ import * as WebRequest from "web-request";
 
 var endpoint = 'http://localhost:8100';
 
+let directoryName : string = "ethan-example1";
+
 // authorization payload
 var payload = {
   "app": {
@@ -44,12 +46,9 @@ interface AuthResponse {
     permissions: Array<string>
 };
 
-async function storeFile(contents: string): Promise<{}> {
-    // let auth : Promise<AuthResponse> =
-
+async function storeFile(contents: string): Promise<boolean> {
     let size : number = contents.length;
-
-    console.log("PING!!")
+    console.log(`storeFile:: contents=${contents}`);
 
     let authRes : AuthResponse =
         await WebRequest.create<AuthResponse>(endpoint + '/auth', {
@@ -65,10 +64,16 @@ async function storeFile(contents: string): Promise<{}> {
         });
 
     // create directory is not idempotant... thanks Obama!
-    await WebRequest.get(endpoint + '/nfs/directory/app/ethan-example')
-        .then( (res) => {
+    let dirExists: boolean =
+        await WebRequest.get(endpoint + `/nfs/directory/app/${directoryName}`, {
+            method: "GET",
+            auth: {
+                bearer: authRes.token
+            }
+        }).then( (res) => {
             if (res.statusCode !== 200) {
-                return WebRequest.post(endpoint + '/nfs/directory/app/ethan-example1', {
+                console.log(res.statusCode);
+                return WebRequest.post(endpoint + `/nfs/directory/app/${directoryName}`, {
                         json: true,
                         method: "POST",
                         auth: {
@@ -78,38 +83,29 @@ async function storeFile(contents: string): Promise<{}> {
                             "isPrivate": true
                         }
                     }).then( (res) => {
-                        console.log("PANG");
                         if (res.statusCode == 200) {
                             console.log("created directory!");
+                            return Promise.resolve(true);
                         } else {
                             throw new SAFEDirCreationError();
                         }
                     });
-            }
-        });
-    /*
-    await WebRequest.post(endpoint + '/nfs/directory/app/ethan-example1', {
-            json: true,
-            method: "POST",
-            auth: {
-                bearer: authRes.token
-            },
-            body: {
-                "isPrivate": true
-            }
-        }).then( (res) => {
-            console.log("PANG");
-            if (res.statusCode == 200) {
-                console.log("created directory!");
             } else {
-                throw new SAFEDirCreationError();
+                console.log("directory exists!!");
+                console.log(res.content);
+                return Promise.resolve(true);
             }
         });
-        */
 
-    await WebRequest.post(endpoint + '/nfs/directory/app/ethan-example/store.txt', {
+    if (!dirExists) {
+        return Promise.resolve(false);
+    }
+
+    let fileOk : boolean =
+        await WebRequest.post(endpoint + `/nfs/file/app/${directoryName}/store.txt`, {
             json: true,
             method: "POST",
+            encoding: null,
             auth: {
                 bearer: authRes.token
             },
@@ -118,16 +114,44 @@ async function storeFile(contents: string): Promise<{}> {
                 'Content-Length': size // why is this in the client??
                 // 'Metadata': new Buffer('sample metadata').toString('base64')
             },
-            body: {
-                contents
-            }
+            body: new Buffer(contents).toString('base64')
+        }).then( (res) => {
+            console.log("wrote file!!");
+            console.log(`statusCode=${res.statusCode}`);
+            console.log(`content=${res.content}`);
+            return Promise.resolve(res.statusCode === 200);
         });
     
-    return Promise.resolve({});
+    return Promise.resolve(fileOk);
 }
 
-function loadFileContents(): string {
-    return "unimplimented";
+async function loadFileContents(): Promise<string> {
+
+    let authRes : AuthResponse =
+        await WebRequest.create<AuthResponse>(endpoint + '/auth', {
+            json: true,
+            method: "POST",
+            body: payload
+        }).response.then( (res) => {
+            if (res.statusCode === 401) {
+                throw new SAFEAuthError();
+            } else {
+                return res.content;
+            }
+        });
+
+    let fileResponse =
+        await WebRequest.get(endpoint + `/nfs/file/app/${directoryName}/store.txt`, {
+            method: "GET",
+            encoding: null, // get us some binary data
+            auth: {
+                bearer: authRes.token
+            }
+        });
+    console.log(`loadFileContents:: statusCode=${fileResponse.statusCode}`);
+    console.log(`loadFileContents:: content=${fileResponse.content}`);
+
+    return Promise.resolve(fileResponse.content);
 }
 
 
@@ -139,7 +163,9 @@ function loadFileContents(): string {
 let args = process.argv.slice(2);
 if (args[0] === "store") {
     storeFile(args[1]);
-} else {
-    let contents = loadFileContents();
-    console.log("contents");
+} else if (args[0] === "load") {
+    let contents : Promise<string> = loadFileContents();
+    contents.then( (cont) => {
+        console.log(`contents=${cont}`);
+    });
 }
