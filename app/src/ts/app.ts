8384472ce127ded3c13528e9ac0d1bc3d6fa865d
Ethan Pailes <ethanpailes@gmail.com>
2016-12-23 13:37:49 -0500

    Added upload script. Right now it is pretty impotent.

---
+++ app/src/ts/app.ts
@@ -0,0 +1,39 @@
+
+
+const electron = require('electron');
+const BrowserWindow = electron.BrowserWindow;
+const app = electron.app;
+
+// const {app, BrowserWindow} = require('electron');
+
+import Config from "./global-config";
+const CONFIG : Config = Config.getInstance();
+
+import { SafeClient } from "safe-launcher-client";
+
+let win;
+
+function createWindow(): void {
+    win = new BrowserWindow({ width: 800, height: 600 });
+
+    console.log(`${__dirname}`);
+    win.loadURL(`file://${__dirname}/index.html`);
+
+    win.webContents.openDevTools();
+
+    let safeClient : SafeClient = new SafeClient(CONFIG.makeAuthPayload(), CONFIG.SAFE_LAUNCHER_ENDPOINT);
+
+    console.log(CONFIG.APP_HOME_DIR);
+
+    win.on('closed', () => {
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
