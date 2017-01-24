f6adbfe772de46f0d5d22855e62c4b996a3b5a27
Ethan Pailes <ethanpailes@gmail.com>
2017-01-24 14:27:02 -0500

    Fixed issue where app would double-authenticate. Actually upload file to network.

--- app/src/ts/upload.ts
+++ app/src/ts/upload.ts
@@ -16,6 +16,7 @@ async function uploadVideo(localVideoFile: string,
         .createFromLocalFile("app", localVideoFile, `videos/${videoTitle}`, "video/ogg")
         .catch( (err) => {
             console.error(err); // TODO: actual error handling
+            console.error(JSON.stringify(err));
         });
 }
 
@@ -27,6 +28,9 @@ $(document).ready(() => {
 
     let video = $("#video-input");
 
+    // ensure videos directory exists
+    safeClient.nfs.dir.create("app", "videos", false);
+
     video.click( () => {
         remote.dialog.showOpenDialog({
             title: "Upload Video",
@@ -36,12 +40,13 @@ $(document).ready(() => {
         });
     });
 
-    $("#submit").click( () => {
+    $("#submit").click(async () => {
         const videoFile = $("#upload-container #video-input").val();
         const title = $("#upload-container #title").val();
         const description = $("#upload-container #desc").val();
 
-        console.log(`${videoFile}, ${title}, ${description}`);
+        console.log(`calling uploadVideo(${videoFile}, ${title}, ${description})`);
+        await uploadVideo(videoFile, title, description);
     });
 
 });
