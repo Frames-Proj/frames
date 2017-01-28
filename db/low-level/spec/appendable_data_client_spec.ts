50c62c5441e2e1e16ea70723b96a6282116934da
Ethan Pailes <ethanpailes@gmail.com>
2017-01-28 12:49:09 -0500

    unfocused test.

--- db/low-level/spec/appendable_data_client_spec.ts
+++ db/low-level/spec/appendable_data_client_spec.ts
@@ -7,22 +7,20 @@ describe("An appendable data client", () => {
 
     it("can create an appendable data", async (done) => {
         const appdat: AppendableDataId =
-            await failDone(client.ad.create("Some name"), done);
+            await failDone(client.ad.create("Some name" + makeid()), done);
         done();
     });
 
 
     it("can convert an appendable-data-id to a data-id", async (done) => {
         const appDataID: AppendableDataId =
-            await failDone(client.ad.create("Some name"), done);
+            await failDone(client.ad.create("Some name" + makeid()), done);
         const dataID: DataIDHandle =
             await failDone(client.ad.toDataIdHandle(appDataID), done);
         done();
     });
 
-    fit("can convert an appendable-data-id to a data-id and back again", async (done) => {
-        // done(); // TODO(ethan): delete me
-
+    it("can convert an appendable-data-id to a data-id and back again", async (done) => {
         const appDataID: AppendableDataId =
             await failDone(client.ad.create("Some name" + makeid()), done);
 
