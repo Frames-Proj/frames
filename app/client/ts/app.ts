a5c6674a8f8a528ef598446a893a38918448b989
Ethan Pailes <ethanpailes@gmail.com>
2017-02-23 13:06:06 -0500

    Added some startup hooks and got the second video test passing (finally).

--- app/client/ts/app.ts
+++ app/client/ts/app.ts
@@ -7,25 +7,26 @@ const app = electron.app;
 // const {app, BrowserWindow} = require('electron');
 
 import Config from "./global-config";
-const CONFIG : Config = Config.getInstance();
+const CONFIG: Config = Config.getInstance();
+
+import startupHook from "./startup-hooks";
 
 import { SafeClient } from "safe-launcher-client";
 
 let win;
 
-function createWindow(): void {
+async function createWindow(): Promise<void> {
 
     let display: any = electron.screen.getPrimaryDisplay().workAreaSize;
     win = new BrowserWindow({ width: display.width, height: display.height });
+
+    // make sure all the right directories and whatnot are there
+    await startupHook();
 
     console.log(`${__dirname}`);
     win.loadURL(`file://${__dirname}/../../index.html`);
 
     win.webContents.openDevTools();
-
-    let safeClient : SafeClient = new SafeClient(CONFIG.makeAuthPayload(), CONFIG.SAFE_LAUNCHER_ENDPOINT);
-
-    console.log(CONFIG.APP_HOME_DIR);
 
     win.on('closed', () => {
         win = null;
