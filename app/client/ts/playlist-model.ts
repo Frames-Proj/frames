cdda7506bb08b39a74917ca1e26e76c646e5af75
Ethan Pailes <ethanpailes@gmail.com>
2017-03-20 14:40:18 -0400

    added a playlist model with tests.

---
+++ app/client/ts/playlist-model.ts
@@ -0,0 +1,162 @@
+
+import { SafeFile, withDropP, DataIDHandle,
+         StructuredDataHandle, TYPE_TAG_VERSIONED,
+         SerializedDataID, AppendableDataHandle,
+         Drop, AppedableDataMetadata, StructuredDataMetadata,
+         FromDataIDHandleResponse
+       } from "safe-launcher-client";
+
+import { safeClient } from "./util";
+const sc = safeClient;
+
+import Video from "./video-model";
+
+//
+// The playlist class will be stored in a structured data with
+// an attached appendable data
+// on the network. Once MaidSafe adds support for updating
+// a file, it could make more sense to just use a file
+// to store things.
+//
+// For now, playlists don't allow for reordering, but a
+// workaround for the future is to allow playlist members
+// to be either videos or reorder nodes. There would have
+// to be a little structural checking to see which one is
+// which. Reorder nodes can provide a mapping from node
+// indexes as they actually appear in the appendable data
+// to the index at which they should appear in the new data.
+//
+// TODO: what are the right ownership semantics here? Should
+// a playlist own its videos?
+export default class Playlist implements Drop {
+    private m_videos: Video[];
+    public get videos() { return this.m_videos; }
+
+    public readonly networkList: AppendableDataHandle;
+    private networkInfo: StructuredDataHandle;
+
+    public readonly title;
+    public readonly description;
+    public readonly owner;
+
+    private constructor(vs: Video[],
+                networkList: AppendableDataHandle,
+                title: string,
+                description: string,
+                owner: string
+               ) {
+        this.m_videos = vs;
+        this.networkList = networkList;
+
+        this.title = title;
+        this.description = description;
+        this.owner = owner;
+    }
+    // this must be called after the constructor
+    private setNetworkInfo(networkInfo: StructuredDataHandle): void {
+        this.networkInfo = networkInfo;
+    }
+
+    public async drop() {
+        await this.networkList.drop();
+        await this.networkInfo.drop();
+    }
+
+    public static async read(xorName: DataIDHandle): Promise<Playlist> {
+        // get the info blob
+        const sdH: StructuredDataHandle =
+           (await sc.structured.fromDataIdHandle(xorName)).handleId;
+        const liS = await sdH.readAsObject();
+        if (!isPlaylistInfoStringy(liS))
+            throw new Error("Malformed PlaylistInfo response.");
+        const pi: PlaylistInfo = toPI(liS);
+
+        // get the actual list of videos
+        const networkVideoListMD: FromDataIDHandleResponse =
+            await withDropP(await sc.dataID.deserialise(pi.videos),
+                            did => sc.ad.fromDataIdHandle(did));
+        const networkVideoList = networkVideoListMD.handleId;
+
+        // NOTE: this loads all the videos. Is that too slow?
+        let videos: Video[] = [];
+        for (let i = 0; i < networkVideoListMD.dataLength; ++i) {
+            videos.push(await Video.read(await networkVideoList.at(i)));
+        }
+
+        let p = new Playlist(videos, networkVideoList, pi.title, pi.description, pi.owner);
+        p.setNetworkInfo(sdH);
+        return p;
+    }
+
+    public static async new(title: string, description: string): Promise<Playlist> {
+        const videoList: AppendableDataHandle = await sc.ad.create(title + " videos");
+        await videoList.save();
+        let p = new Playlist([], videoList, title, description, "TODO OWNER");
+        p.setNetworkInfo(await p.write());
+        return p;
+    }
+
+    // helper function for this.new()
+    private async write(): Promise<StructuredDataHandle> {
+        const info: PlaylistInfoStringy = toPIStringy({
+            title: this.title,
+            description: this.description,
+            owner: this.owner,
+            videos: await withDropP(await this.networkList.toDataIdHandle(), di => di.serialise())
+        });
+        const piS = await sc.structured.create(this.title, TYPE_TAG_VERSIONED, info);
+        await piS.save();
+        return piS;
+    }
+
+    public async append(video: Video): Promise<void> {
+        this.m_videos.push(video);
+        await withDropP(await video.xorName, di => this.networkList.append(di));
+    }
+
+    public xorName(): Promise<DataIDHandle> {
+        return this.networkInfo.toDataIdHandle();
+    }
+}
+
+//
+// Playlist info boilerplate
+//
+interface PlaylistInfoBase {
+    title: string;
+    description: string;
+    owner: string;
+}
+function isPlaylistInfoBase(x: any): x is PlaylistInfoBase {
+    return  ( typeof x.title === "string"
+              && typeof x.description === "string"
+              && typeof x.owner === "string");
+}
+interface PlaylistInfoStringy extends PlaylistInfoBase {
+    videos: string; // base64 encoded
+}
+function isPlaylistInfoStringy(x: any): x is PlaylistInfoStringy {
+    return (typeof x.videos === "string") && isPlaylistInfoBase(x);
+}
+function toPI(vi: PlaylistInfoStringy): PlaylistInfo {
+    return {
+        title: vi.title,
+        description: vi.description,
+        owner: vi.owner,
+        videos: Buffer.from(vi.videos, "base64"),
+    };
+}
+interface PlaylistInfo extends PlaylistInfoBase {
+    videos: SerializedDataID;
+}
+function isVideoInfo(x: any): x is PlaylistInfo {
+    return x.videos instanceof Buffer && isPlaylistInfoBase(x);
+}
+function toPIStringy(vi: PlaylistInfo): PlaylistInfoStringy {
+    return {
+        title: vi.title,
+        description: vi.description,
+        owner: vi.owner,
+        videos: vi.videos.toString("base64")
+    };
+}
---
+++ app/client/ts/spec/playlist-model-spec.ts
@@ -0,0 +1,107 @@
+
+import { TEST_DATA_DIR, failDone, makeid, checkForLeakErrors } from "./test-util";
+
+import Playlist from "../playlist-model";
+import Video from "../video-model";
+
+import Config from "../global-config";
+const CONFIG: Config = Config.getInstance();
+
+import { TYPE_TAG_VERSIONED, DataIDHandle,
+         SerializedDataID, withDropP, StructuredDataHandle,
+         setCollectLeakStats, setCollectLeakStatsBlock
+       } from "safe-launcher-client";
+import { safeClient } from "../util";
+const sc = safeClient;
+
+import startupHook from "../startup-hooks";
+
+describe("A playlist model", () => {
+
+    beforeAll(async (done) => {
+        await failDone(startupHook(), done);
+        setCollectLeakStats();
+        done();
+    });
+
+    afterAll(() => {
+        checkForLeakErrors();
+    });
+
+    it("can be freshly created.", async (done) => {
+        setCollectLeakStatsBlock("pms:test1 create new playlist");
+
+        const playlist = await failDone(Playlist.new("Cats " + makeid(), "A list of cat videos"), done);
+        await failDone(playlist.drop(), done);
+        done();
+    });
+
+    it("can be read from an XorName", async (done) => {
+        setCollectLeakStatsBlock("pms:test2 round trip");
+
+        const playlist = await failDone(Playlist.new("Cats " + makeid(), "A list of cat videos"), done);
+        const xorName: SerializedDataID =
+            await failDone(withDropP(await playlist.xorName(), di => di.serialise()), done);
+
+        const playlist2: Playlist =
+            await failDone(withDropP(await sc.dataID.deserialise(xorName), di => Playlist.read(di)), done);
+
+        expect(playlist.title).toBe(playlist2.title);
+        expect(playlist.description).toBe(playlist2.description);
+        expect(playlist.owner).toBe(playlist2.owner);
+
+        await failDone(playlist.drop(), done);
+        await failDone(playlist2.drop(), done);
+        done();
+    });
+
+    it("can get a video appended", async (done) => {
+        setCollectLeakStatsBlock("pms:test3 append");
+
+        const video: Video =
+            await failDone(Video.new("title " + makeid(), "A description.",
+                                    `${TEST_DATA_DIR}/test-vid.mp4`), done);
+        const playlist = await failDone(Playlist.new("Cats " + makeid(), "A list of cat videos"), done);
+
+        await failDone(playlist.append(video), done);
+        await failDone(playlist.append(video), done);
+
+        await failDone(video.drop(), done);
+        await failDone(playlist.drop(), done);
+        done();
+    });
+
+    it("can be serialized with videos in the list", async (done) => {
+        setCollectLeakStatsBlock("pms:test4 append round trip");
+
+        const video: Video =
+            await failDone(Video.new("title " + makeid(), "A description.",
+                                    `${TEST_DATA_DIR}/test-vid.mp4`), done);
+        const playlist = await failDone(Playlist.new("Cats " + makeid(), "A list of cat videos"), done);
+
+        await failDone(playlist.append(video), done);
+        await failDone(playlist.append(video), done);
+
+        const xorName: SerializedDataID =
+            await failDone(withDropP(await playlist.xorName(), di => di.serialise()), done);
+
+        const playlist2: Playlist =
+            await failDone(withDropP(await sc.dataID.deserialise(xorName), di => Playlist.read(di)), done);
+
+        const video2: Video = playlist2.videos[0];
+        expect(video.title).toBe(video2.title);
+        expect(video.description).toBe(video2.description);
+        expect(video.owner).toBe(video2.owner);
+
+        expect(playlist.title).toBe(playlist2.title);
+        expect(playlist.description).toBe(playlist2.description);
+        expect(playlist.owner).toBe(playlist2.owner);
+
+        await playlist2.videos.forEach(async v => await v.drop());
+        await failDone(playlist2.drop(), done);
+        await failDone(video.drop(), done);
+        await failDone(playlist.drop(), done);
+        done();
+    });
+
+});
