44e79635f3eefc98b86de316f9cfa7eae01827c2
Abdisalan <abdi.mohamud94@gmail.com>
2017-04-17 14:45:38 -0400

    removed the longname abstraction component

--- app/client/components/SignIn.tsx
+++ app/client/components/SignIn.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
 import { Jumbotron } from 'react-bootstrap';
 import { LongNameForm } from './LongName';
+import { prepareSignIn, addLongName, updateLongName } from '../../client/ts/signIn-utils';
 
 export class SignIn extends React.Component<{}, {}> {
+
+    componentDidMount() {
+        prepareSignIn.bind(this)();
+    }
     render() {
         return (
             <div>
@@ -13,7 +18,15 @@ export class SignIn extends React.Component<{}, {}> {
                     <h1>SignIn</h1>
                     <p>Choose a longName to represent you</p>
                 </Jumbotron>
-                <LongNameForm/>
+                <div style={{padding: "50px"}}>
+                    <h2>Select or Create a LongName</h2>
+                    <p>Select a LongName here</p>
+                    <select ref="nameDropdown" onChange={updateLongName.bind(this)}></select>
+                    <p> Or you can create a longname </p>
+                    <input type="text" ref="longName"/>
+                    <p ref="error_msg"></p>
+                    <button onClick={addLongName.bind(this)}>Add this Long Name</button>
+                </div>
             </div>
         )
     }
