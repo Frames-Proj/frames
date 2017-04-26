1236737873aa345f6e202c13611e1be8d3b4e0f4
Graham Goudeau <grahamgoudeau@gmail.com>
2017-04-26 12:04:52 -0400

    Implement cache for video data

---
+++ app/client/ts/video-cache.ts
@@ -0,0 +1,65 @@
+import Video from "./video-model";
+import Config from "./global-config";
+
+const CONFIG: Config = Config.getInstance();
+
+interface Cache {
+    [xorName: string]: {
+        video: Video,
+        accessTime: Date
+    }
+}
+
+export default class VideoCache {
+    private static INSTANCE: VideoCache = null;
+    private cache: Cache;
+    private numEntries: number;
+
+    private constructor() {
+        this.cache = {};
+        this.numEntries = 0;
+    }
+
+    static getInstance() {
+        if (VideoCache.INSTANCE == null) {
+            VideoCache.INSTANCE = new VideoCache();
+        }
+        return VideoCache.INSTANCE;
+    }
+
+    async getFromXorName(xorName: string) {
+        if (this.cache[xorName] == null) {
+            if (this.numEntries + 1 > CONFIG.MAX_CACHE_SIZE) {
+
+                // sort by oldest first
+                const oldKeys: string[] = Object.keys(this.cache).sort((key1: string, key2: string) => {
+                    if (this.cache[key1].accessTime < this.cache[key2].accessTime) return -1;
+                    else if (this.cache[key1].accessTime > this.cache[key2].accessTime) return 1;
+                    else return 0;
+                });
+
+                // delete half of the oldest cache entries
+                for (let i = 0; i < CONFIG.MAX_CACHE_SIZE / 2; i++) {
+                    delete this.cache[oldKeys[i]];
+                }
+                this.numEntries = Object.keys(this.cache).length;
+            }
+
+            const video: Video = await Video.readFromStringXorName(xorName);
+            this.cache[xorName] = {
+                video: video,
+                accessTime: new Date()
+            };
+
+            this.numEntries += 1;
+        } else {
+            this.cache[xorName].accessTime = new Date();
+        }
+        return this.cache[xorName].video;
+    }
+
+    invalidateCache() {
+        this.cache = {};
+        this.numEntries = 0;
+    }
+}
